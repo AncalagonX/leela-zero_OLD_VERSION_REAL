88c339cef285d87564c2436649c971f1a0dee7d4
AncalagonX <37827291+AncalagonX@users.noreply.github.com>
2018-03-28 17:06:27 +0900

    Typo fix

--- src/NNCache.cpp
+++ src/NNCache.cpp
@@ -78,7 +78,7 @@ void NNCache::set_size_from_playouts(int max_playouts) {
     // size based on playouts increases the hit rate while balancing memory
     // usage for low playout instances. 50'000 cache entries is ~250 MB
 	auto max_size = std::min(50'000, std::max(6'000, 3 * max_playouts)); // ORIGINAL LINE
-    auto max_size = 500'000; // MY NEW LINE, OVERRIDES PREVIOUS LINE
+    max_size = 500'000; // MY NEW LINE, OVERRIDES PREVIOUS LINE
     NNCache::get_NNCache().resize(max_size);
 }
 
