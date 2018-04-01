39197cd5308ac1541e64254cff4d5d375da0a69d
AncalagonX <37827291+AncalagonX@users.noreply.github.com>
2018-04-01 17:08:28 +0900

    Set "dump_stats" back to 500, 500 for testing.

--- src/UCTSearch.cpp
+++ src/UCTSearch.cpp
@@ -1006,7 +1006,7 @@ int UCTSearch::think(int color, passflag_t passflag) {
     // display search info
     myprintf("\n");
 
-    dump_stats(m_rootstate, *m_root, 10, 100, true);
+    dump_stats(m_rootstate, *m_root, 500, 500, true);
     Training::record(m_rootstate, *m_root);
 
     Time elapsed;
@@ -1073,7 +1073,7 @@ void UCTSearch::ponder() {
 	pondering_now = false;
     // display search info
     myprintf("\n");
-    dump_stats(m_rootstate, *m_root, 10, 100, true);
+    dump_stats(m_rootstate, *m_root, 500, 500, true);
 
     myprintf("\n%d visits, %d nodes\n\n", m_root->get_visits(), m_nodes.load());
 }
